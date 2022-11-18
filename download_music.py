6123d7db3af83e068aff48bf03a0167e10314627
Toby Chen <atch84@gmail.com>
2022-11-18 23:12:59 +0800

    download music

--- download_music.py
+++ download_music.py
@@ -5,6 +5,8 @@ from googleapiclient.errors import HttpError
 from pytube import YouTube
 import os
 
+DOWNLOAD_STATUS = '/home/ubuntu/download_status'
+MUSIC_META = '/home/ubuntu/music_meta'
 
 def get_music(vid):
     url = f"https://www.youtube.com/watch?v={vid}"
@@ -14,21 +16,35 @@ def get_music(vid):
     if vid + ".mp3" not in os.listdir('/home/ubuntu/music/'):
         yt.streams.get_audio_only().download(filename=_filename)
 
-DEVELOPER_KEY = ''
-youtube = build('youtube', 'v3', developerKey=DEVELOPER_KEY)
+def download_music():
+    with open('/home/ubuntu/.dev_key',  'r') as file:
+        developerKey = file.read().strip()
+    youtube = build('youtube', 'v3', developerKey=developerKey)
+
+    if os.path.exists(MUSIC_META):
+        music_meta = json.load(open(MUSIC_META, 'r'))
+    else:
+        music_meta = {}
+
+    r = requests.get('https://kma.kkbox.com/charts/api/v1/yearly?category=297&lang=tc&limit=100&terr=tw&type=newrelease&year=2022')
+
+    song_name = []
+    for song in json.loads(r.text)['data']['charts']['newrelease']:
+        song_name.append(song['song_name'])
 
 
+    for idx, song in enumerate(song_name):
+        with open(DOWNLOAD_STATUS, 'w') as file:
+            file.write(f'{idx}/{len(song_name)}')
 
-r = requests.get('https://kma.kkbox.com/charts/api/v1/yearly?category=297&lang=tc&limit=100&terr=tw&type=newrelease&year=2022')
+        request = youtube.search().list(part="snippet",q=song)
+        response = request.execute()
 
-song_name = []
-for song in json.loads(r.text)['data']['charts']['newrelease']:
-    song_name.append(song['song_name'])
+        vid = response['items'][0]['id']['videoId']
+        get_music(vid)
+        music_meta[vid] = {'name': song, 'volume': 100}
+        json.dump(music_meta, open(MUSIC_META, 'w'))
 
 
-for song in song_name:
-
-    request = youtube.search().list(part="snippet",q=song)
-    response = request.execute()
-
-    get_music(response['items'][0]['id']['videoId'])
+if __name__ == '__maiu__':
+    download_music()
--- mflask.py
+++ mflask.py
@@ -1,5 +1,6 @@
 from flask import Flask
-
+from download_music import download_music, DOWNLOAD_STATUS
+from threading import Thread
 
 app = Flask(__name__)
 
@@ -16,6 +17,18 @@ def stop():
     with open('/home/ubuntu/mstatus', 'w') as file:
         file.write('stop')
     return 'stop'
+
+@app.route('/download')
+def download():
+    download_thread = Thread(target=download_music)
+    download_thread.start()
+    return 'start!'
+
+@app.route('/download_status')
+def download_status():
+    with open(DOWNLOAD_STATUS, 'r') as file:
+        status = file.read()
+    return status
 
 
 if __name__ == '__main__':
